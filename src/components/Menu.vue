a4fbd5289bebf58402294e17179eb8a93a85c22b
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-04-19 02:00:13 +0900

    init: add blog (initial version) powered by vue.js

---
+++ src/components/Menu.vue
@@ -0,0 +1,40 @@
+<template>
+    <Slide>
+      <a href="/">
+        <span class="navi-item">Home</span>
+      </a>
+      <a href="/about">
+        <span class="navi-item">About</span>
+      </a>
+    </Slide>
+</template>
+
+<script>
+import { Slide } from 'vue-burger-menu' // import the CSS transitions you wish to use, in this case we are using `Slide`
+export default {
+  name: 'app',
+  components: {
+    Slide
+  }
+}
+</script>
+
+<style scoped>
+.navi-item:hover {
+  opacity: 0.5;
+}
+@media screen and (max-width: 767px) {
+  .bm-burger-button {
+    width: 24px !important;
+    height: 20px !important;
+    left: 20px !important;
+  }
+}
+@media screen and (min-width: 768px) {
+  .bm-burger-button {
+    width: 36px;
+    height: 30px;
+    left: 36px;
+  }
+}
+</style>
