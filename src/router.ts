12e5bfa4d52d01f7ce534608b0ff2aab2489ab90
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-04-19 18:28:12 +0900

    [add] firebase

--- src/router.ts
+++ src/router.ts
@@ -9,12 +9,12 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/blog',
+      path: '/',
       name: 'home',
       component: Home,
     },
     {
-      path: '/blog/about',
+      path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
