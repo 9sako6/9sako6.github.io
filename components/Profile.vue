9775ca9948719a0814b8b5766e060c674292b3c9
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-11-26 17:09:29 +0900

    edit prof

--- components/Profile.vue
+++ components/Profile.vue
@@ -5,7 +5,10 @@
       <img id="profile-avatar" src="~/static/icon.png" />
     </div>
     <div class="side-description">
-      くさころです。RubyとC++で競プロをしています。正直競プロあまり得意じゃないです。自分でもわかるようになるべく行間のない解説を心がけます。
+      くさころです。RubyとC++で競プロをしています。
+      競プロあまり得意じゃないです。
+      自分でもわかるようになるべく行間のない解説を心がけます。
+      好きなアルゴリズムはBFSです。
       <ul>
         <li class="list-item">
           <a href="https://atcoder.jp/users/qsako6">AtCoder</a>
