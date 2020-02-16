b4b76880ab182ed88b5bbc2593c699bcd482405d
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-02-17 02:39:40 +0900

    update profile description

--- components/Profile.vue
+++ components/Profile.vue
@@ -5,9 +5,12 @@
       <img id="profile-avatar" src="~/static/icon.png" alt="a picture drawn by 9sako6" />
     </div>
     <div class="side-description">
-      <p>くさころ(9sako6)といいます。</p>
-      <p>名古屋大学 -> 名古屋大学大学院在学中。</p>
-      <p>当サイト「庭に腐ったコロッケ」では、Web開発と競技プログラミングの情報を発信します。</p>
+      <div>くさころ(9sako6)といいます。名前の由来は腐ったコロッケです。</div>
+      <div>名古屋大学 → 名古屋大学大学院在学中。</div>
+      <div>
+        当サイト「庭に
+        <div class="frame">腐</div>ったコロッケ」では、Web開発と競技プログラミングの情報を発信します。
+      </div>
       <ul>
         <li v-for="item in items" :key="item.id">
           <a :href="item.link" target="_blank" rel="noopener noreferrer">{{item.title}}</a>
@@ -30,11 +33,19 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 @import "@/assets/css/side.css";
 #profile-avatar {
   width: 200px;
   border-radius: 100%;
   text-align: center;
+}
+.frame {
+  width: 22px;
+  text-align: center;
+  display: inline-block;
+  background-color: $my-black;
+  color: $my-white;
+  transform: rotate(-20deg);
 }
 </style>
