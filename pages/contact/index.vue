37bf1bafa1a7886e6d7b52ed3f7d6187733d5a9e
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-02-16 20:17:59 +0900

    fix contact page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,14 +1,22 @@
 <template>
   <section class="page">
+    <div id="page-title">お問い合わせ</div>
+    <h1>Twitter</h1>
+    <p>
+      <a href="https://twitter.com/9sako6" target="_blank" rel="noopener noreferrer">@9sako6</a>
+    </p>
     <h1>サイトからのお問い合わせ</h1>
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSf6NQGAawQ5eeB1urTJml5U-M_nxOBW0AoY6JdUHrvodULVEw/viewform?embedded=true"
-      width="100%"
-      height="1200"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
-    >読み込んでいます…</iframe>
+    <p>
+      <nuxt-link to="/policy">プライバシーポリシー</nuxt-link>を確認した上で送信してください。
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSf6NQGAawQ5eeB1urTJml5U-M_nxOBW0AoY6JdUHrvodULVEw/viewform?embedded=true"
+        width="100%"
+        height="1200"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >読み込んでいます…</iframe>
+    </p>
   </section>
 </template>
 <script>
