6aefc17c754d2022515bcc25fe9b5b6c854b7d56
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-08-19 23:37:55 +0900

    Show draft posts in development env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -155,6 +155,7 @@ export default {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+    CTF_PREVIEW_ACCESS_TOKEN: process.env.CTF_PREVIEW_ACCESS_TOKEN,
     TWITTER_USER: process.env.TWITTER_USER
   },
   styleResources: {
