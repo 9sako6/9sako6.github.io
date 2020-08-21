95cd7e193e2426de303c6d1eaae13c9c9e3950f2
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-08-22 03:39:06 +0900

    Add target: static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ const siteTitle = '腐ったコロッケ';
 
 export default {
   mode: 'universal',
+  target: 'static',
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
