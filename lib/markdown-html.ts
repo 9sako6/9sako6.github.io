8d2d23cc550f407c766029a0947e8be0df649033
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-01-22 10:26:29 +0900

    Support Katex

---
+++ lib/markdown-html.ts
@@ -0,0 +1,18 @@
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkMath from "remark-math";
+import remarkRehype from "remark-rehype";
+import rehypeKatex from "rehype-katex";
+import rehypeStringify from "rehype-stringify";
+
+export const markdownToHtml = async (markdown: string) => {
+  return (
+    await unified()
+      .use(remarkParse)
+      .use(remarkMath)
+      .use(remarkRehype)
+      .use(rehypeKatex)
+      .use(rehypeStringify)
+      .process(markdown)
+  ).toString();
+};
