d0bbb7344fa4fcd0063f169433402a53c3511636
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-04-11 00:19:04 +0900

    style: Add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "@/styles/globals.css";
+import "@/styles/nprogress.css";
 import "@/styles/rehype-prism-plus.css";
 import type { AppProps } from "next/app";
+import Router from "next/router";
+import nProgress from "nprogress";
 import { ThemeProvider } from "next-themes";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
