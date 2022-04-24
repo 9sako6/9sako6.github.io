4a443be926dc768c9fcee3233c82b5e354a96195
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-04-24 21:20:56 +0900

    Set meta information to all pages

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
-import { Header, Footer } from "../organisms";
+import { Header, Footer } from "@/components/organisms";
+import Head from "next/head";
 
 type Props = {
   children?: ReactNode;
@@ -8,6 +9,19 @@ type Props = {
 export const Layout = ({ children }: Props): JSX.Element => {
   return (
     <div className="pl-4 pr-4">
+      <Head>
+        <title>{process.env.siteTitle}</title>
+        <meta name="description" content={process.env.siteDescription} />
+        <meta property="og:title" content={process.env.siteTitle} />
+        <meta property="og:description" content={process.env.siteDescription} />
+        <meta
+          property="og:image"
+          content={`${process.env.siteUrl}/icon.webp`}
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@9sako6" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <main className="flex items-center justify-center">
         <div className="md:w-2xl">{children}</div>
