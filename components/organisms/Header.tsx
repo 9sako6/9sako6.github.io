c1c5909c7e2ba621e6ecda2746cd04bcecf10185
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-07-19 23:14:31 +0900

    Add a side bar in PC (#247)

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -6,7 +6,7 @@ import { SiteTitle } from "@/components/atoms/SiteTitle";
 export const Header = (): JSX.Element => {
   return (
     <header>
-      <nav className="flex items-center pb-20 pt-20 md:max-w-2xl m-auto justify-between">
+      <nav className="flex items-center pb-20 pt-20 md:max-w-4xl m-auto justify-between">
         <Link href="/">
           <a>
             <SiteTitle />
--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,3 +1,4 @@
 export { Footer } from "./Footer";
 export { Header } from "./Header";
 export { History } from "./History";
+export { SideBar } from "./SideBar";
