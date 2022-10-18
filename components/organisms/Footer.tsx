ed7f37e8001561a3c409283f9adb604a83aca5bd
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-10-19 03:13:19 +0900

    Update footer

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -23,17 +23,21 @@ export const Footer = (): JSX.Element => {
           <div className="grid md:grid-cols-4 md:gap-4">
             <div className="">
               <div className="flex items-center gap-2">
-                <div className="w-16 h-16 relative">
-                  <Image
-                    alt="icon"
-                    className="rounded-full"
-                    src={"/icon.webp"}
-                    layout={"fill"}
-                    objectFit={"contain"}
-                  />
-                </div>
+                <Link href="/about">
+                  <a>
+                    <div className="w-16 h-16 relative hover:scale-105">
+                      <Image
+                        alt="icon"
+                        className="rounded-full"
+                        src={"/icon.webp"}
+                        layout={"fill"}
+                        objectFit={"contain"}
+                      />
+                    </div>
+                  </a>
+                </Link>
                 <nav className="flex gap-2 text-3xl cursor-pointer">
-                  <div className="hover:text-zinc-600 hover:dark:text-zinc-200">
+                  <div className="hover:text-zinc-600 hover:dark:text-zinc-200 hover:scale-105">
                     <a
                       href="https://github.com/9sako6"
                       target="_blank"
@@ -43,7 +47,7 @@ export const Footer = (): JSX.Element => {
                       <GitHubIcon />
                     </a>
                   </div>
-                  <div className="hover:text-zinc-600 hover:dark:text-zinc-200">
+                  <div className="hover:text-zinc-600 hover:dark:text-zinc-200 hover:scale-105">
                     <a
                       href="https://twitter.com/9sako6"
                       target="_blank"
