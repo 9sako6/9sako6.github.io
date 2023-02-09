4e8f7713daf0114eaca4cbbec2875b4fad494311
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-02-09 23:28:42 +0900

    Goodbye Storybook
    メンテできない

--- .storybook/nextMdxMock.js
+++
@@ -1,7 +0,0 @@
-// NOTE: storybook が @next/mdx を読み込めずビルドに失敗することへの対策。
-// next.config.js で使われる @next/mdx をモックしている。
-module.exports = function () {
-  return function (arg) {
-    return arg;
-  };
-};
