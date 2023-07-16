755da8fe85e262b2ca0bc2071bd230bf8946b1d6
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-07-16 18:39:40 +0900

    Replace about page with app router

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import AboutContent from "@/components/features/contents/about.mdx";
+import { MarkdownWrapper } from "@/components/ui/MarkdownWrapper";
+
+const AboutPage = () => (
+  <MarkdownWrapper>
+    <AboutContent />
+  </MarkdownWrapper>
+);
+
+export default AboutPage;
