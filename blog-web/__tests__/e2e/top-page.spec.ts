6d47d7d0f1307c925a1ad7d5dde79b5a7baa53dc
Yutaro Taira <31821663+9sako6@users.noreply.github.com>
2023-12-10 23:25:53 +0900

    Fix test

--- blog-web/__tests__/e2e/top-page.spec.ts
+++ blog-web/__tests__/e2e/top-page.spec.ts
@@ -9,7 +9,7 @@ test("top page has a valid title and a link to the about page", async ({
   await expect(page).toHaveTitle(/腐ったコロッケ/);
 
   // create a locator
-  const aboutPage = page.getByText("A brief resume is here.");
+  const aboutPage = page.getByText("私のプロフィールはこちら。");
 
   // Expect an attribute "to be strictly equal" to the value.
   await expect(aboutPage).toHaveAttribute("href", "/about");
@@ -17,5 +17,4 @@ test("top page has a valid title and a link to the about page", async ({
   await aboutPage.click();
 
   await expect(page.locator(".markdown")).toHaveText(/経歴/);
-  await expect(page.locator(".markdown")).toHaveText(/Education/);
 });
