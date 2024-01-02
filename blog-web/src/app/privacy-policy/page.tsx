ff88c3a4318aab9541b8936ebb52a486730c48c9
Yutaro Taira <9sako6@gmail.com>
2024-01-02 18:25:29 +0900

    Add plivacy policy

---
+++ blog-web/src/app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Content from "@/components/features/contents/privacy-policy.mdx";
+import { MarkdownWrapper } from "@/components/ui/MarkdownWrapper";
+
+const AboutPage = () => (
+  <MarkdownWrapper>
+    <Content />
+  </MarkdownWrapper>
+);
+
+export default AboutPage;
--- blog-web/src/components/ui/Footer/Footer.tsx
+++ blog-web/src/components/ui/Footer/Footer.tsx
@@ -19,71 +19,74 @@ export const Footer = async () => {
       <div className="h-40 flex justify-center items-center">
         <BackToTop />
       </div>
-      <div className="md:m-auto">
-        <div className="grid md:grid-cols-4 md:gap-4">
-          <div>
-            <div className="flex items-center gap-2">
-              <nav className="flex gap-2 text-3xl cursor-pointer">
-                <div className={styles.iconContainer}>
-                  <a
-                    href="https://github.com/9sako6"
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label="link to GitHub"
-                  >
-                    <GitHubIcon />
-                  </a>
-                </div>
-                <div className={styles.iconContainer}>
-                  <a
-                    href="https://twitter.com/9sako6"
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label="link to X"
-                  >
-                    <XIcon />
-                  </a>
-                </div>
-                <div className={styles.iconContainer}>
-                  <a
-                    href="/rss.xml"
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label="RSS"
-                  >
-                    <RSSIcon />
-                  </a>
-                </div>
-              </nav>
-            </div>
-            <div className="py-6 break-word">
-              <p>
-                Web アプリケーションの開発をして生きている
-                <ruby>
-                  9sako6<rp>(</rp>
-                  <rt>くさころ</rt>
-                  <rp>)</rp>
-                </ruby>
-                です。
-              </p>
-              <p>
-                <Link href="/about" className={styles.link}>
-                  私のプロフィールはこちら。
-                </Link>
-              </p>
-            </div>
+      <div className="grid md:grid-cols-4 md:gap-4">
+        <section>
+          <div className="flex items-center gap-2">
+            <nav className="flex gap-2 text-3xl cursor-pointer">
+              <div className={styles.iconContainer}>
+                <a
+                  href="https://github.com/9sako6"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="link to GitHub"
+                >
+                  <GitHubIcon />
+                </a>
+              </div>
+              <div className={styles.iconContainer}>
+                <a
+                  href="https://twitter.com/9sako6"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="link to X"
+                >
+                  <XIcon />
+                </a>
+              </div>
+              <div className={styles.iconContainer}>
+                <a
+                  href="/rss.xml"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="RSS"
+                >
+                  <RSSIcon />
+                </a>
+              </div>
+            </nav>
           </div>
-          <div className="md:col-span-2 py-6 md:py-0">
-            <TagsList tags={tags} />
+          <div className="py-6 break-word">
+            <p>
+              Web アプリケーションの開発をして生きている
+              <ruby>
+                9sako6<rp>(</rp>
+                <rt>くさころ</rt>
+                <rp>)</rp>
+              </ruby>
+              です。
+            </p>
+            <p>
+              <Link href="/about" className={styles.link}>
+                私のプロフィールはこちら。
+              </Link>
+            </p>
           </div>
-          <div className={styles.archiveListContainer}>
-            <h2 className={styles.archiveListHeader}>年別アーカイブ</h2>
-            <ArchiveList />
-          </div>
-        </div>
+        </section>
+        <section className="md:col-span-2 py-6 md:py-0">
+          <TagsList tags={tags} />
+        </section>
+        <section className={styles.archiveListContainer}>
+          <h2 className={styles.archiveListHeader}>年別アーカイブ</h2>
+          <ArchiveList />
+        </section>
       </div>
-      <div className="text-sm flex items-center justify-center text-slate-500 dark:text-zinc-400 p-12">
-        &copy; {year} 9sako6
+      <div className="text-sm flex flex-col items-center justify-center text-slate-500 p-16">
+        <div className={styles.policyContainer}>
+          <Link href="/privacy-policy" className={styles.policyLink}>
+            プライバシーポリシー
+          </Link>
+        </div>
+        <p>&copy; {year} 9sako6</p>
       </div>
     </footer>
   );
