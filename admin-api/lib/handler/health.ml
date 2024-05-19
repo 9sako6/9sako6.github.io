3a4b621723b10c4932aab9abef1b93ff1c40de43
Yutaro Taira <9sako6@gmail.com>
2024-05-19 16:20:59 +0900

    Delete unused

--- admin-api/lib/handler/health.ml
+++
@@ -1,2 +0,0 @@
-let health_handler _request =
-  {| {"status" : "healthy"} |} |> Dream.json
--- e2e/admin-api-e2e/config/api.ts
+++
@@ -1,3 +0,0 @@
-const port = 8080;
-const hostname = "localhost";
-export const apiPath = `http://${hostname}:${port}`;
